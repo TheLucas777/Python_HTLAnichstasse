00e0ee4bde139dede9a52a33f40e79e3a21f30d2
TheLucas777 <64582502+TheLucas777@users.noreply.github.com>
2022-01-17 08:46:40 +0100

    17_01_2021
    adding wiederholung

--- Schulübungen/wiederholung_2.py
+++ Schulübungen/wiederholung_2.py
@@ -16,7 +16,20 @@ ui = int(input("Bitte eine int Zahl eingeben: "))
 
 
 #if-elif-else-statements
-if (ui > 0):
+if(ui > 0):
     print("Dein Input ist groesser als 0")
 else:
-    
+    print("Dein Input ist kleiner als 0")
+
+#Schleifen
+x = 6
+while(x > 0):
+    print(x)
+    x = x - 1
+
+for x in range(12,2,-2):
+    print(x)
+
+s2 = "Hallo liebe Leute!"
+for buchstabe in s2:
+    print(buchstabe)
