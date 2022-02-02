adeffc511213f3a70f64e0c38386b2925198c2c6
TheLucas777 <64582502+TheLucas777@users.noreply.github.com>
2022-02-02 15:29:14 +0100

    added Game lotto.py

---
+++ Hausübungen/lotto.py
@@ -0,0 +1,47 @@
+'''
+vierte Hausübung - Themen: Lotto
+User guesses 6 numbers in range 1-45 and
+if the numbers are equal to 6 random numbers than he won
+Lucas
+02.02.2022
+'''
+import random
+
+debugMode = True
+# two variables numbers guesses
+numbers = []
+guesses = []
+counter = 1
+
+
+# Check how many are right
+def check(list1, list2):
+    right = 0
+
+    if (debugMode):
+        print("[DEBUG] Coparing l1 = {} and l2 = {}".format(list1, list2))
+
+    for x in list1:
+        if x in list2:
+            right = right + 1
+    return right
+
+
+# six random number to a list
+for x in range(0, 6, 1):
+    rand = random.randint(1, 45)
+    numbers.append(rand)
+
+    if (debugMode):
+        print("[DEBUG] Generated number {} it is {}.        List: {}".format(x, rand, numbers))
+
+# user guesses six numbers
+for x in range(0, 6, 1):
+    guesses.append(int(input("[Lotto] Please set your {}. number --> ".format(counter))))
+    counter = counter + 1
+
+# win or lose
+if (check(numbers, guesses) == 6):
+    print("[Lotto] You guessed EVERY number!! Congrats!")
+else:
+    print("[Lotto] You guessed {} number(s) right.".format(check(numbers, guesses)))
