e253c6990d64043d7d51597cba4a1f448859a269
TheLucas777 <64582502+TheLucas777@users.noreply.github.com>
2022-02-28 09:36:04 +0100

    added Strucktogrammehausübung

---
+++ Hausübungen/strucktogramme_2würfel.py
@@ -0,0 +1,25 @@
+'''
+Themen: Strucktogramme
+Strucktogramm:
+Lucas
+28.02.2022
+'''
+import random
+
+# initiating variables
+counter = 0
+dice1 = 0
+dice2 = 0
+augenpaare = []
+
+while (True):
+    if (dice1 != 6 or dice2 != 6):
+        dice1 = random.randint(1, 6)
+        dice2 = random.randint(1, 6)
+        augenpaare.append(str(dice1) + "+" + str(dice2))
+        counter = counter +1
+        print(counter)
+    else:
+        break
+print("beide Würfel haben nach {} Versuche(n) eine 6 gewürfelt".format(counter))
+print("Die vorherigen Augenpaare waren {}".format(augenpaare))
