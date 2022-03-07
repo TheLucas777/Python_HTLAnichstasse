f8f03f26db810b637f7bc931fdd9cfdaf71c7f80
TheLucas777 <64582502+TheLucas777@users.noreply.github.com>
2022-03-07 09:11:32 +0100

    started Funktions in 008.py

--- Schulübungen/008.py
+++ Schulübungen/008.py
@@ -3,3 +3,12 @@ sechstes Übungsfile - Themen: Funktionen
 Lucas
 24.01.2022
 '''
+
+#simple Funktion
+def hi():
+    print("Hello World")
+
+#Normal Program starts here.
+ui = int(input("Enter a number please -> "))
+for x in range(ui):
+    hi()
